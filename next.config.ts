f6d431016e737d5dd987fa56f74d2adc8ca5b892
jbbalmontO2 <132301654+jbbalmont@users.noreply.github.com>
2025-04-07 07:05:07 -0400

    Fix: fix vercel Cicd

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-}
+  webpack: (config, { isServer }) => {    if (!isServer) {
+      config.externals.push('fs', 'path', 'os');
+    }
+
+    config.module.rules.push({
+      test: /backend/,
+      use: 'null-loader',
+    });
+
+    return config;
+  },
+};
+
 
 export default nextConfig;
